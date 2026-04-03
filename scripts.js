091b5b747a6bdfd5accb11e1a52ae41fb05b9244
Sopuru <platoon1277@gmail.com>
2026-04-03 13:51:44 +0100

    the function was made to update the dom

--- scripts.js
+++ scripts.js
@@ -5,20 +5,38 @@ const button = document.querySelector("button");
 
 function gridSize(e) {
   e.preventDefault();
-    let number = prompt("Enter Your Number");
-    let NumberInput = parseInt(number);
-    if (NumberInput > 100) {
-      container.textContent = "Error!";
-    } else {
-      NumberInput;
-      const totalCells = NumberInput * NumberInput;
+  let number = prompt("Enter Your Number");
+  let NumberInput = parseInt(number);
+  if (NumberInput > 100) {
+    container.textContent = "Error!";
+  } else {
+    container.innerHTML = "";
+    NumberInput;
+    const totalCells = NumberInput * NumberInput;
 
-      for (let index = 1; index < totalCells; index++) {
-        const SquaredDiv1 = document.createElement("div");
-        SquaredDiv1.classList.add("div1");
-        container.append(SquaredDiv1);
-      }
+    for (let index = 1; index < totalCells; index++) {
+      const SquaredDiv1 = document.createElement("div");
+      SquaredDiv1.classList.add("div1");
+      container.append(SquaredDiv1);
     }
-  };
+  }
+}
 
 button.addEventListener("click", gridSize);
+// button.addEventListener("click", () => {
+//   // e.preventDefault();
+//   let number = prompt("Enter Your Number");
+//   let NumberInput = parseInt(number);
+//   if (NumberInput > 100) {
+//     container.textContent = "Error!";
+//   } else {
+//     NumberInput;
+//     const totalCells = NumberInput * NumberInput;
+
+//     for (let index = 1; index < totalCells; index++) {
+//       const SquaredDiv1 = document.createElement("div");
+//       SquaredDiv1.classList.add("div1");
+//       container.append(SquaredDiv1);
+//     }
+//   }
+// });
