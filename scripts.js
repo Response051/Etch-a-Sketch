c20031cb1d673b7f6c31538ae930a7fd2e085838
Sopuru <platoon1277@gmail.com>
2026-04-03 13:28:22 +0100

    the addEventListerner and prompt input was updated

--- scripts.js
+++ scripts.js
@@ -1,11 +1,24 @@
 console.log("hello World");
 
 const container = document.querySelector("#container");
-// const gridSize = prompt('enter your Number',  );
-const totalCells = gridSize * gridSize;
+const button = document.querySelector("button");
 
-for (let index = 1; index < totalCells; index++) {
-  const SquaredDiv1 = document.createElement("div");
-  SquaredDiv1.classList.add("div1");
-  container.append(SquaredDiv1);
-}
+function gridSize(e) {
+  e.preventDefault();
+    let number = prompt("Enter Your Number");
+    let NumberInput = parseInt(number);
+    if (NumberInput > 100) {
+      container.textContent = "Error!";
+    } else {
+      NumberInput;
+      const totalCells = NumberInput * NumberInput;
+
+      for (let index = 1; index < totalCells; index++) {
+        const SquaredDiv1 = document.createElement("div");
+        SquaredDiv1.classList.add("div1");
+        container.append(SquaredDiv1);
+      }
+    }
+  };
+
+button.addEventListener("click", gridSize);
